7e94a72a11cc3902753d84dd4bca7d8753ca1635
Artsiom Martsinovich <Artsiom.Martsinovich@ge.com>
2025-03-02 21:00:41 +0300

    adding loader & refactoring

---
+++ src/components/Loader.jsx
@@ -0,0 +1,39 @@
+import Spinner1 from './Spinner1.jsx';
+import Spinner2 from './Spinner2.jsx';
+import '../styles/Loader.css';
+
+function Loader({ text, spinner, background }) {
+  if (!text && !spinner) {
+    return null;
+  }
+
+  return (
+    <div className="loader">
+      {background === true ? (
+        <div className="loader-back">
+          <div className="loader-content">
+            {text && <div className="loader-text">{text}</div>}
+            {spinner === 1 && <Spinner1 />}
+            {spinner === 2 && (
+              <Spinner2
+                style={text ? { transform: 'translateY(0.15em)' } : null}
+              />
+            )}
+          </div>
+        </div>
+      ) : (
+        <div className="loader-content">
+          {text && <div className="loader-text">{text}</div>}
+          {spinner === 1 && <Spinner1 />}
+          {spinner === 2 && (
+            <Spinner2
+              style={text ? { transform: 'translateY(0.15em)' } : null}
+            />
+          )}
+        </div>
+      )}
+    </div>
+  );
+}
+
+export default Loader;
--- src/components/Spinner2.jsx
+++ src/components/Spinner2.jsx
@@ -1,19 +1,19 @@
 import { useEffect, useRef } from 'react';
 import '../styles/Spinner2.css';
 
-function Spinner2() {
+function Spinner2({ style }) {
   const spinnerRef = useRef();
 
   useEffect(() => {
     const dots = spinnerRef.current.children;
 
     for (let i = 1; i < dots.length; i++) {
-      dots[i].style.animationDelay = `${i * 70}ms`;
+      dots[i].style.animationDelay = `${i * 80}ms`;
     }
   }, []);
 
   return (
-    <div className="spinner-2" ref={spinnerRef}>
+    <div style={style} className="spinner-2" ref={spinnerRef}>
       <div className="spinner-dot"></div>
       <div className="spinner-dot"></div>
       <div className="spinner-dot"></div>
